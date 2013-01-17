9fd6f7e9c0d96cfa5b8c04cf6b59358817cbb66b
Mashalla <mashalla@mashalla-virtual-machine.(none)>
2013-01-17 12:34:04 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cannon::Application.config.secret_token = '1115b9e7d73887c606ae425f208e3604da72674ccbfb98bce707eb72edc4501e8093c1145f8bfeeaed1a3a3e22cdebe9a9f8c613c638a85b250ef23a799cc473'
